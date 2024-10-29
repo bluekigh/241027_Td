d9bf8c5cf6c50da20cd1a92d96cbc2962709a3d0
Simsungbo <ppc993@naver.com>
2024-10-29 15:17:04 +0900

    Enemy Spawn&Move

--- Assets/1.Script/StageWayPoint.cs
+++ Assets/1.Script/StageWayPoint.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class StageWayPoint : MonoBehaviour
 {
     public Vector3[] waypoint = new Vector3[] { };
-    private void Start() {
+    
+    private void OnEnable()
+    {
         App.Instance.Waypoint = waypoint;
     }
 }
