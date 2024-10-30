dbdabbb7921584b63384eef432f0c0998f05ab0d
audrms94 <audrms747@naver.com>
2024-10-30 14:05:56 +0900

    1_heroprefab2_remove

--- Assets/1.Script/Hero.cs
+++ Assets/1.Script/Hero.cs
@@ -13,7 +13,7 @@ public class Hero : MonoBehaviour
     public GameObject hero_prefab1;  //영웅 레벨1 프리펩 받아오는 변수
     public GameObject hero_prefab2;  //레벨2 프리펩 받아오는 변수
     //public GameObject tile;
-
+    public LayerMask layermask;      //타일 레이어 받아오기
 
 
     void Start()
@@ -29,50 +29,46 @@ public class Hero : MonoBehaviour
 
     void Gold_Check(BuildAble tile_build)   //돈 체크 - 돈이 충분하다면 타워 생성
     {
-        
         int num = tile_build.BuildLV();
-        Debug.Log("초기num"+num);
-        if (App.Instance.BuyTower(1) == true)
+        if (num == 0)
         {
-            if (num == 0)
+            
+            if (App.Instance.BuyTower(1) == true) //타워 비용 소모
             {
-                Debug.Log("1차num" + num);
-                lev_1();    //레벨 1 타워 생성
-                tile_build.build();
+                lev_1(tile_build); //레벨 1 타워 생성
             }
-            else if (num == 1)   //레벨2 타워 생성
+
+
+        }
+        else if (num == 1)   //레벨2 타워 생성
+        {
+           
+            if (App.Instance.BuyTower(1) == true)
             {
-                Debug.Log("2차num" + num);
-                lev_2();
-                tile_build.build();
-            }
-            else if(num == 2)
-            {
-                return;
+                lev_2(tile_build);
             }
 
         }
-      
-        else
+        else if (num == 2)
         {
-            print("돈이 부족합니다");
+            Debug.Log("해당 지역에 설치할 수 없습니다.");
+            return;
         }
-
     }
-    void lev_1()
+    void lev_1(BuildAble tile_build)
     {
         Debug.Log("레벨1 타워생성");
         //Instantiate(this.hero_prefab1, heropos, Quaternion.identity);  
         GameObject temp1 = Instantiate(this.hero_prefab1, heropos, Quaternion.identity); //해당 지점 좌표에 타워(Hero) 생성(Instantiate)
-        BuildAble b_mathod = FindObjectOfType<BuildAble>();
-        b_mathod.build(temp1);
+        //BuildAble b_mathod = FindObjectOfType<BuildAble>();
+        tile_build.build(temp1);
     }
-    void lev_2() 
+    void lev_2(BuildAble tile_build)
     {
         Debug.Log("레벨2 타워생성");
         GameObject temp2 = Instantiate(this.hero_prefab2, heropos, Quaternion.identity);
-        BuildAble b_mathod = FindObjectOfType<BuildAble>();
-        b_mathod.build(temp2);
+        //BuildAble b_mathod = FindObjectOfType<BuildAble>();
+        tile_build.build(temp2);
     }
     void Hero_Create()  //Hero 생성
     {
@@ -83,8 +79,8 @@ public class Hero : MonoBehaviour
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit = new RaycastHit();
 
-            
-            if (Physics.Raycast(ray, out hit) == true)
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, layermask) == true) //특정 레이어의 오브젝트만 찾고 나머지는 관통
             {
                 heropos = hit.transform.position; //충돌 지점을 heropos에 저장
                 print("클릭 위치" + heropos);
@@ -99,5 +95,5 @@ public class Hero : MonoBehaviour
         }
     }
 
-    
+
 }
