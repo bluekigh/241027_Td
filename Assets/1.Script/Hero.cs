19dce5654d026f89c7d602294754cd0607393b5a
audrms94 <audrms747@naver.com>
2024-10-30 19:17:42 +0900

    1_sounds_2_effects

--- Assets/1.Script/Hero.cs
+++ Assets/1.Script/Hero.cs
@@ -8,13 +8,14 @@ using static UnityEditor.ObjectChangeEventStream;
 
 public class Hero : MonoBehaviour
 {
-    Vector3 heropos;                //영웅의 생성 위치(클릭 위치)
+    Vector3 heropos;                                      //영웅의 생성 위치(클릭 위치)
 
-    public GameObject hero_prefab1;  //영웅 레벨1 프리펩 받아오는 변수
-    public GameObject hero_prefab2;  //레벨2 프리펩 받아오는 변수
-    //public GameObject tile;
-    public LayerMask layermask;      //타일 레이어 받아오기
-    List<GameObject> Towers = new List<GameObject>();
+    public GameObject hero_prefab1;                       //영웅 레벨1 프리펩 받아오는 변수
+    public GameObject hero_prefab2;                       //레벨2 프리펩 받아오는 변수
+
+    public LayerMask layermask;                           //타일 레이어 받아오기
+
+    List<GameObject> Towers = new List<GameObject>();     //생성된 타워 프리펩을 저장할 리스트
 
     void Start()
     {
@@ -37,17 +38,14 @@ public class Hero : MonoBehaviour
         int num = tile_build.BuildLV();
         if (num == 0)
         {
-            
             if (App.Instance.BuyTower(1) == true) //타워 비용 소모
             {
                 lev_1(tile_build); //레벨 1 타워 생성
             }
-
-
         }
         else if (num == 1)   //레벨2 타워 생성
         {
-           
+
             if (App.Instance.BuyTower(1) == true)
             {
                 lev_2(tile_build);
@@ -63,9 +61,7 @@ public class Hero : MonoBehaviour
     void lev_1(BuildAble tile_build)
     {
         Debug.Log("레벨1 타워생성");
-        //Instantiate(this.hero_prefab1, heropos, Quaternion.identity);  
         GameObject temp1 = Instantiate(this.hero_prefab1, heropos, Quaternion.identity); //해당 지점 좌표에 타워(Hero) 생성(Instantiate)
-        //BuildAble b_mathod = FindObjectOfType<BuildAble>();
         tile_build.build(temp1);
         Towers.Add(temp1);
     }
@@ -73,27 +69,22 @@ public class Hero : MonoBehaviour
     {
         Debug.Log("레벨2 타워생성");
         GameObject temp2 = Instantiate(this.hero_prefab2, heropos, Quaternion.identity);
-        //BuildAble b_mathod = FindObjectOfType<BuildAble>();
         tile_build.build(temp2);
         Towers.Add(temp2);
     }
     void Hero_Create()  //Hero 생성
     {
 
-        if (Input.GetMouseButtonDown(0) == true)
-        //laycast 만들기 -해당 지점 좌표 측정(print)
+        if (Input.GetMouseButtonDown(0) == true)        //laycast 만들기 -해당 지점 좌표 측정(print)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit = new RaycastHit();
 
-
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, layermask) == true) //특정 레이어의 오브젝트만 찾고 나머지는 관통
             {
-                heropos = hit.transform.position; //충돌 지점을 heropos에 저장
-                print("클릭 위치" + heropos);
-                //이펙트 설정
-                if (hit.transform.gameObject.layer == 10)
-                //layer 10번을 설치타일로 설정
+                heropos = hit.transform.position;           //충돌 지점을 heropos에 저장
+                print("클릭 위치" + heropos);                //이펙트 설정
+                if (hit.transform.gameObject.layer == 10)   //layer 10번을 설치타일로 설정
                 {
                     BuildAble tile_build = hit.collider.GetComponent<BuildAble>();
                     Gold_Check(tile_build);
--- Assets/1.Script/hero_prefab.cs
+++ Assets/1.Script/hero_prefab.cs
@@ -13,30 +13,31 @@ public enum Hero_State //영웅 상태
 public class hero_prefab : MonoBehaviour
 {
     Hero_State h_state;                  //영웅 상태 받기
-
+    public AudioSource locationsound;    //타워 설치 오디오 받는 변수
+    public AudioSource AttackSound;      //공격 오디오 받는 변수
+    public GameObject Shootprefab;       //타격 이펙트
 
     Vector3 mypos;                       //영웅의 현재 위치(생성된 영웅)
+    Vector3 enemyvector;                 //가장 가까운 적의 vector값을 받는 변수
     Animator animator;                   //애니메이터 정보
     Transform closestenemy;              //가장 가까운 적군 객체를 저장할 변수
+    
+    Coroutine a_coroutine;               //공격 코루틴
 
     public LayerMask layerMask;          //레이어를 받는 변수
-    //public GameObject Enemy;           //적군의 정보를 받기
-    //float lefttime = 0f;               //적군 생성까지 남은 시간
+
     float e_distance;                    //적군까지의 거리를 받는 변수
-
     public float Range = 1.0f;           //영웅의 사거리
-    public float attack_delay ;    //공격의 딜레이
+    public float attack_delay;           //공격의 딜레이
     public int attack_damage = 1;        //영웅의 공격력
-    //float elapseTime = 0;              //시간 값을 받는 변수
-    //float attackDelay = 2f;            //공격의 딜레이
 
-    Coroutine a_coroutine;                      //공격 코루틴
     void Start()
     {
         mypos = this.transform.position;
         animator = this.GetComponentInChildren<Animator>();
         animator.SetTrigger("Start");
         h_state = Hero_State.FindEnemy; //영웅의 첫 상태
+        
     }
 
     // Update is called once per frame
@@ -48,7 +49,6 @@ public class hero_prefab : MonoBehaviour
 
     void Hero_Proc()    //타워의 상태(상태별 애니메이션 필요)
     {
-        //print("영웅 상태" + h_state);
         switch (h_state)
         {
             case Hero_State.FindEnemy: //정지상태(FindEnemy)
@@ -61,76 +61,55 @@ public class hero_prefab : MonoBehaviour
         }
     }
 
-    
-
     void Attack()
     {
-        //print(" attack.0");
-        //StartCoroutine(Attack_motion());
-
-        //lefttime += Time.deltaTime;
-
-        //if(lefttime >= attack_delay)
-        //{
-        //    animator.SetTrigger("Attack");
-        //    closestenemy.GetComponent<Enemy_Damage>().TakeDamage(attack_damage);  //데미지 주는 함수
-        //    lefttime = 0f; 
-        //}
-
-        if (a_coroutine == null) { a_coroutine = StartCoroutine(Attack_routine()); }
-        
-       
-        
-        
+        if (a_coroutine == null)
+        {
+            a_coroutine = StartCoroutine(Attack_routine());
+        }
     }
     IEnumerator Attack_routine() //타격이 끝나고 나면 일정 시간 후에 재타격
     {
-
-        //First_Attack();
-        //while (e_distance <= Range)
-        
-            First_Attack();
-            yield return new WaitForSeconds(attack_delay);
-            Debug.Log("공격");
-            h_state = Hero_State.FindEnemy;
-            a_coroutine = null;
-        
+        First_Attack();
+        yield return new WaitForSeconds(attack_delay);
+        Debug.Log("공격");
+        h_state = Hero_State.FindEnemy;
+        a_coroutine = null;
     }
     void First_Attack()
     {
-        if (closestenemy != null) 
+        if (closestenemy != null)
         {
-            //closestenemy.GetComponent<Enemy_Damage>().TakeDamage(attack_damage);
+            Instantiate(Shootprefab, enemyvector, Quaternion.identity);
+            AttackSound.Play();
             animator.SetTrigger("Attack");
             Invoke("Add_Damage", 0.3f);
+            
         }
-        
     }
     void Add_Damage()
     {
         if (closestenemy != null)
         {
             closestenemy.GetComponent<Enemy_Damage>().TakeDamage(attack_damage);
+            
         }
     }
 
-
     void FindEnemy()  //근처의 적 검색탐지
     {
-        closestenemy =null;
+        closestenemy = null;
         Collider[] hitcolliders = Physics.OverlapSphere(mypos, Range, layerMask);  //12번이 Enemy 라는 전제하  //범위(range) 설정
-        //print("근처 적의 수" + hitcolliders.Length);
-
 
         if (hitcolliders.Length > 0) //탐지 범위 내에 들어오면
         {
-
             foreach (Collider collider in hitcolliders)
             {
                 float distance = Vector3.Distance(transform.position, collider.transform.position);
                 e_distance = distance;
-                closestenemy = collider.transform;  //가장 가까운 적 설정
-                
+                closestenemy = collider.transform;  //가장 가까운 적 객체 설정
+                enemyvector = collider.transform.position;  //가장 가까운 적의 vector값
+
                 if (distance <= Range)  //적 타격
                 {
                     h_state = Hero_State.Attack;
@@ -146,37 +125,4 @@ public class hero_prefab : MonoBehaviour
             h_state = Hero_State.FindEnemy;
         }
     }
-    
-    //void searching_enemy()
-    //{
-    //    if (Enemy != null)
-    //    {
-    //        mypos = this.transform.position;
-    //        float search_dist = Vector3.Distance(mypos, Enemy.transform.position);
-
-    //        if (search_dist <= Range)   //탐지거리 이내면
-    //        {
-    //            h_state = Hero_State.Attack;
-    //        }
-    //        else
-    //        {
-    //            h_state = Hero_State.FindEnemy;
-    //        }
-    //    }
-    //    else 
-    //    {
-    //        h_state = Hero_State.FindEnemy;
-    //    }
-    //}
 }
-/*
- * 
-
-
-
-
-
-
-공격에 속성이 들어감 - 기존의 타워보다 성장한 능력치의 스크립드를 새로운 프리펩에 적용
-
-*/
