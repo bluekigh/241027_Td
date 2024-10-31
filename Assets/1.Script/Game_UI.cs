1038ec897ed1df88550b9a7d0d4c9bd262545813
blueligh <blueligh@naver.com>
2024-10-31 11:40:49 +0900

    UI Update

--- Assets/1.Script/Game_UI.cs
+++ Assets/1.Script/Game_UI.cs
@@ -6,16 +6,20 @@ public class Game_UI : MonoBehaviour
 {
     public Text goldtext;
     public Text hptext;
+    public Text StageText;
     // public Button BtnGold;
     // public Button BtnHp;
-
+    int stageNum = 0;
     private void Start()
     {
+        stageNum = 0;
+        StageText.text = "Stage 1";
         App.Instance.goldChnage = goldtextChage;
         App.Instance.hpChnage = hptextChage;
         App.Instance.goldChnage.Invoke();
         App.Instance.hpChnage.Invoke();
-       
+        App.Instance.changestage += changeStage;
+
         // BtnGold.onClick.AddListener(() =>
         // {
         //     App.Instance.Gold--;
@@ -25,6 +29,17 @@ public class Game_UI : MonoBehaviour
         //     App.Instance.HP--;
         // });
     }
+
+    private void changeStage()
+    {
+        if (stageNum == 0)
+        {
+            StageText.text = "Stage 2";
+            stageNum++;
+        }
+        else return;
+    }
+
     private void OnDisable()
     {
         App.Instance.goldChnage -= goldtextChage;
